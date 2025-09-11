fc47cc06462ed412f6e94d2ad11ebf874ee15083
alokMIPL <alok@madhulata.com>
2025-09-11 15:21:15 +0530

    code added

---
+++ src/components/GoogleAnalytics.jsx
@@ -0,0 +1,28 @@
+'use client'
+
+import Script from 'next/script'
+
+const GoogleAnalytics = () => {
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-JYF8QR1YC8"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JYF8QR1YC8');
+          `,
+        }}
+      />
+    </>
+  )
+}
+
+export default GoogleAnalytics
