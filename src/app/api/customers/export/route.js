908344815a325e49d33e332f8f178aeb991172b0
alokMIPL <alok@madhulata.com>
2025-10-16 16:52:25 +0530

    Customer export section code added

---
+++ src/app/api/customers/export/route.js
@@ -0,0 +1,32 @@
+import { isAuthenticated } from "@/lib/authentication";
+import { connectDB } from "@/lib/db";
+import { catchError, response } from "@/lib/helperFunction";
+import UserModel from "@/models/User.model";
+
+export async function GET(request) {
+  try {
+    const auth = await isAuthenticated("admin");
+    if (!auth.isAuth) {
+      return response(false, 403, "Unauthorized Or Not Admin.");
+    }
+
+    await connectDB();
+
+    const filter = {
+      deletedAt: null,
+    };
+
+    const getCustomers = await UserModel.find(filter)
+      .select("-media -description")
+      .sort({ createdAT: -1 })
+      .lean();
+
+    if (!getCustomers) {
+      return response(false, 403, "Collection Empty.");
+    }
+
+    return response(true, 200, "Data Found.", getCustomers);
+  } catch (error) {
+    return catchError(error);
+  }
+}
