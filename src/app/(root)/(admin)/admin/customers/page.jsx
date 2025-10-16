e974e11955912689b4e97ebf3c20e51111bd1da2
alokMIPL <alok@madhulata.com>
2025-10-16 16:08:57 +0530

    minor code added

--- src/app/(root)/(admin)/admin/customers/page.jsx
+++ src/app/(root)/(admin)/admin/customers/page.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+"use client";
+import BreadCrumb from "@/components/Application/Admin/BreadCrumb";
+import DatatableWrapper from "@/components/Application/Admin/DatatableWrapper";
+import DeleteAction from "@/components/Application/Admin/DeleteAction";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { DT_CUSTOMERS_COLUMN } from "@/lib/column";
+import { columnConfig } from "@/lib/helperFunction";
+import { ADMIN_DASHBOARD, ADMIN_TRASH } from "@/routes/AdminPanelRoute";
+import { useCallback, useMemo } from "react";
+
+const breadCrumbData = [
+  { href: ADMIN_DASHBOARD, label: "Home" },
+  { href: "", label: "Customers" },
+];
 
 const ShowCustomers = () => {
-  return (
-    <div>ShowCustomers</div>
-  )
-}
+  const columns = useMemo(() => {
+    return columnConfig(DT_CUSTOMERS_COLUMN);
+  }, []);
 
-export default ShowCustomers
+  const action = useCallback((row, deleteType, handleDelete) => {
+    let actionMenu = [];
+    actionMenu.push(
+      <DeleteAction
+        key="delete"
+        handleDelete={handleDelete}
+        row={row}
+        deleteType={deleteType}
+      />
+    );
+    return actionMenu;
+  }, []);
+
+  return (
+    <div>
+      <BreadCrumb breadCrumbData={breadCrumbData} />
+      <Card className="py-0 rounded shadow-sm gap-0">
+        <CardHeader className="pt-3 px-3 border-b [.border-b]:pb-2">
+          <div className="flex justify-between items-center">
+            <h4 className="text-xl font-semibold">Customers</h4>
+          </div>
+        </CardHeader>
+        <CardContent className="pb-5 px-0">
+          <DatatableWrapper
+            queryKey="customers"
+            fetchUrl="/api/customers"
+            initialPageSize={10}
+            columnsConfig={columns}
+            exportEndpoint="/api/customers/export"
+            deleteEndpoint="/api/customers/delete"
+            deleteType="SD"
+            trashView={`${ADMIN_TRASH}?trashof=customers`}
+            createAction={action}
+          />
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default ShowCustomers;
