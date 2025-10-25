48bcf01baf05b366889034b26317735381f02955
alokMIPL <alok@madhulata.com>
2025-10-25 15:55:03 +0530

    variant image code added

--- src/app/(root)/(website)/product/[slug]/ProductDetails.jsx
+++ src/app/(root)/(website)/product/[slug]/ProductDetails.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -10,6 +10,8 @@ import {
 } from "@/components/ui/breadcrumb";
 import { WEBSITE_PRODUCT_DETAILS, WEBSITE_SHOP } from "@/routes/WebsiteRoute";
 import Link from "next/link";
+import Image from "next/image";
+import imagePlaceholder from "@/assets/images/img-placeholder.webp";
 
 const ProductDetails = ({ product, variant, colors, sizes, reviewCount }) => {
   // console.log("product",product)
@@ -17,6 +19,16 @@ const ProductDetails = ({ product, variant, colors, sizes, reviewCount }) => {
   // console.log("colors",colors)
   // console.log("sizes",sizes)
   // console.log("reviewCount",reviewCount)
+
+  const [activeThumb, setActiveThumb] = useState();
+
+  useEffect(() => {
+    setActiveThumb(variant?.media?.[0]?.secure_url);
+  }, [variant]);
+
+  const handleThumb = (thumbUrl) => {
+    setActiveThumb(thumbUrl);
+  };
 
   return (
     <div className="lg:px-32 px-4">
@@ -32,12 +44,47 @@ const ProductDetails = ({ product, variant, colors, sizes, reviewCount }) => {
             </BreadcrumbItem>
             <BreadcrumbSeparator />
             <BreadcrumbItem>
-            <BreadcrumbLink>
-              <Link href={WEBSITE_PRODUCT_DETAILS(product?.slug)}  >{product?.name}</Link>
-            </BreadcrumbLink>
+              <BreadcrumbLink>
+                <Link href={WEBSITE_PRODUCT_DETAILS(product?.slug)}>
+                  {product?.name}
+                </Link>
+              </BreadcrumbLink>
             </BreadcrumbItem>
           </BreadcrumbList>
         </Breadcrumb>
+      </div>
+      <div className="md:flex justify-between items-start lg:gap-10 gap-5 mb-20">
+        <div className="md:w-1/2 xl:flex xl:justify-center xl:gap-5 md:sticky md:top-0">
+          <div className="xl:order-last xl:mb-0 mb-5 xl:w-[calc(100%-144px)]">
+            <Image
+              src={activeThumb || imagePlaceholder.src}
+              width={650}
+              height={650}
+              alt="product"
+              className="border rounded max-w-full"
+            />
+          </div>
+          <div className="flex xl:flex-col items-center xl:gap-5 gap-3 xl:w-36 overflow-auto xl:pb-0 pb-2 max-h-[600]">
+            {variant?.media?.map((thumb) => (
+              <Image
+                key={thumb._id}
+                src={thumb?.secure_url || imagePlaceholder.src}
+                width={100}
+                height={100}
+                alt="product thumbnail"
+                className={`md:max-w-full max-w-16 rounded cursor-pointer ${
+                  thumb.secure_url === activeThumb
+                    ? "border-2 border-primary "
+                    : "border"
+                } `}
+                onClick={() => handleThumb(thumb?.secure_url)}
+              />
+            ))}
+          </div>
+        </div>
+        <div>
+          
+        </div>
       </div>
     </div>
   );
