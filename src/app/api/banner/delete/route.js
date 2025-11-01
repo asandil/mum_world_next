a97ec0c4ec239142dc12f774eacebf457452f577
alokMIPL <alok@madhulata.com>
2025-11-01 19:07:22 +0530

    code added

--- src/app/api/banner/delete/route.js
+++ src/app/api/banner/delete/route.js
@@ -7,7 +7,7 @@ export async function PUT(request) {
   try {
     const auth = await isAuthenticated("admin");
     if (!auth.isAuth) {
-      return response(false, 403, "Unauthorized Or Not an Admin.");
+      return response(false, 403, "Unauthorized Or Not an Admin");
     }
 
     await connectDB();
@@ -55,3 +55,40 @@ export async function PUT(request) {
   }
 }
 
+export async function DELETE(request) {
+  try {
+    const auth = await isAuthenticated("admin");
+    if (!auth.isAuth) {
+      return response(false, 403, "Unauthorized Or Not an Admin");
+    }
+
+    await connectDB();
+
+    const payload = await request.json();
+    const ids = payload.ids || [];
+    const deleteType = payload.deleteType;
+
+    if (!Array.isArray(ids) || ids.length === 0) {
+      return response(false, 400, "Invalid or empty id list.");
+    }
+
+    const data = await BannerModel.find({ _id: { $in: ids } }).lean();
+    if (!data.length) {
+      return response(false, 404, "Data Not Found.");
+    }
+
+    if (!deleteType === "PD") {
+      return response(
+        false,
+        404,
+        "Invalid delete operation. Delete type should be PD for this route."
+      );
+    }
+
+    await BannerModel.deleteMany({ _id: { $in: ids } });
+
+    return response(true, 200, "Data deleted permanently.");
+  } catch (error) {
+    return catchError(error);
+  }
+}
