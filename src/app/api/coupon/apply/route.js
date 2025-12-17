1b0abc76acea26fb195e3d22c35c22e0e4e71d47
alokMIPL <alok@madhulata.com>
2025-12-17 23:59:38 +0530

    apply coupon api code added

---
+++ src/app/api/coupon/apply/route.js
@@ -0,0 +1,41 @@
+import { connectDB } from "@/lib/db";
+import { catchError, response } from "@/lib/helperFunction";
+import { zSchema } from "@/lib/zodSchema";
+import CouponModel from "@/models/Coupon.model";
+
+export async function POST(request) {
+  try {
+    await connectDB();
+    const payload = await request.json();
+    const couponFormSchema = zSchema.pick({
+      code: true,
+      minShoppingAmount: true,
+    });
+
+    const validate = couponFormSchema.safeParse(payload)
+    if(!validate.success){
+      return response(false, 400, "Missing or invalid data.", validate.error)
+    }
+
+    const {code, minShoppingAmount} = validate.data;
+
+    const couponData = await CouponModel.findOne({code}).lean();
+
+    if(!couponData){
+      return response(false, 400, "Invalid or expired coupon code.")
+    }
+
+    if(new Date() > couponData.validate){
+      return response(false, 400, "Expired coupon code ")
+    }
+
+    if(minShoppingAmount < couponData.minShoppingAmount){
+      return response(false, 400, "In-sufficient shopping amount.")
+    }
+
+    return response(true, 200, "coupon applied succesfully.", {discountPercentage:couponData.discountPercentage})
+
+  } catch (error) {
+    return catchError(error);
+  }
+}
