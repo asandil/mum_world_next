f12ae87df2646b86113cc257834298511980d6e5
alokMIPL <alok@madhulata.com>
2026-01-09 23:58:54 +0530

    code added

--- src/app/(root)/(website)/profile/page.jsx
+++ src/app/(root)/(website)/profile/page.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+"use client"
+import { ButtonLoading } from "@/components/Application/ButtonLoading";
+import UserPanelLayout from "@/components/Application/website/UserPanelLayout";
+import WebsiteBreadcrumb from "@/components/Application/website/WebsiteBreadcrumb";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zSchema } from "@/lib/zodSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const breadCrumbData = {
+  title: "Profile",
+  links: [{ label: "Profile" }],
+};
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
 
-export default Profile
+  const [loading, setLoading] = useState(false)
+
+  const formSchema = zSchema.pick({
+    name:true, 
+    email: true,
+    address: true
+  });
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      address: ""
+    },
+  });
+
+  const updateProfile = (value) => {
+
+  }
+
+  return (
+    <div>
+      <WebsiteBreadcrumb props={breadCrumbData} />
+      <UserPanelLayout>
+        <div className="shadow rounded">
+          <div className="p-5 text-xl font-semibold border-b">Profile</div>
+          <div className="mt-5">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(updateProfile)}
+                className="space-y-8"
+              >
+                <div className="mb-5">
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="email"
+                            placeholder="mumWorld@gmail.com"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="mb-3">
+                    <ButtonLoading loading={loading} type="submit" text="Save Changes" className="w-full cursor-pointer" />
+                </div>
+              </form>
+            </Form>
+          </div>
+        </div>
+      </UserPanelLayout>
+    </div>
+  );
+};
+
+export default Profile;
