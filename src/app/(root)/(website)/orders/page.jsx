bc70f77409d9ff1db7d1361a1a6a1050680338b1
alokMIPL <alok@madhulata.com>
2026-01-09 17:51:22 +0530

    User Order code added

--- src/app/(root)/(website)/orders/page.jsx
+++ src/app/(root)/(website)/orders/page.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+"use client";
+import UserPanelLayout from "@/components/Application/website/UserPanelLayout";
+import WebsiteBreadcrumb from "@/components/Application/website/WebsiteBreadcrumb";
+import useFetch from "@/hooks/useFetch";
+import { WEBSITE_ORDER_DEATILS } from "@/routes/WebsiteRoute";
+import Link from "next/link";
+import React from "react";
+import { HiOutlineShoppingBag } from "react-icons/hi2";
+import { IoCartOutline } from "react-icons/io5";
+import { useSelector } from "react-redux";
+
+const breadCrumbData = {
+  title: "Orders",
+  links: [{ label: "Orders" }],
+};
 
 const Orders = () => {
   return (
-    <div>Orders</div>
-  )
-}
+    <div>
+      Orders
+    </div>
+  );
+};
 
-export default Orders
+export default Orders;
--- src/components/Application/website/UserPanelNavigation.jsx
+++ src/components/Application/website/UserPanelNavigation.jsx
@@ -7,7 +7,12 @@ import axios from 'axios'
 import Link from 'next/link'
 import { usePathname, useRouter } from 'next/navigation'
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux';
+
+import { HiOutlineShoppingBag } from "react-icons/hi2";
+import { TbLayoutDashboardFilled } from "react-icons/tb";
+import { FaUserCog } from "react-icons/fa";
+import { LuLogOut } from "react-icons/lu";
 
 const UserPanelNavigation = () => {
   const pathname = usePathname();
@@ -31,16 +36,28 @@ const UserPanelNavigation = () => {
     <div className="border shadow-sm p-4 rounded">
       <ul>
         <li className="mb-2" >
-          <Link href={USER_DASHBOARD} className={`block p-3 text-sm rounded hover:bg-primary hover:text-white ${pathname.startsWith(USER_DASHBOARD) ? "bg-primary text-white" : "" }`}>Dashboard</Link>
+          <Link href={USER_DASHBOARD} className={`flex items-center gap-2 p-3 text-sm rounded hover:bg-primary hover:text-white transition-colors ${pathname.startsWith(USER_DASHBOARD) ? "bg-primary text-white" : "" }`}>
+           <TbLayoutDashboardFilled size={28}/>
+            <span>Dashboard</span>
+           </Link>
         </li>
         <li className="mb-2" >
-          <Link href={USER_PROFILE} className={`block p-3 text-sm rounded hover:bg-primary hover:text-white ${pathname.startsWith(USER_PROFILE) ? "bg-primary text-white" : "" }`}>Profile</Link>
+          <Link href={USER_PROFILE} className={`flex items-center gap-2 p-3 text-sm rounded hover:bg-primary hover:text-white ${pathname.startsWith(USER_PROFILE) ? "bg-primary text-white" : "" }`}>
+          <FaUserCog size={28}/>
+            <span>Profile</span>
+          </Link>
         </li>
         <li className="mb-2" >
-          <Link href={USER_ORDERS} className={`block p-3 text-sm rounded hover:bg-primary hover:text-white ${pathname.startsWith(USER_ORDERS) ? "bg-primary text-white" : "" }`}>Orders</Link>
+          <Link href={USER_ORDERS} className={`flex items-center gap-2 p-3 text-sm rounded hover:bg-primary hover:text-white ${pathname.startsWith(USER_ORDERS) ? "bg-primary text-white" : "" }`}>
+          <HiOutlineShoppingBag size={28} />
+          <span>Orders</span>
+          </Link>
         </li>
         <li className="mb-2" >
-          <Button type="button" onClick={handleLogOut} variant="destructive" className="w-full">LogOut</Button>
+          <Button type="button" onClick={handleLogOut} variant="destructive" className="w-full flex items-center justify-center gap-2">
+            <LuLogOut size={28} />
+            <span>LogOut</span>
+          </Button>
         </li>
       </ul>
     </div>
