7e4c2e0cf3a770e3c6cf5e64c201c5839ba3030a
alokMIPL <alok@madhulata.com>
2026-01-10 13:24:46 +0530

    code added

--- src/app/api/profile/update/route.js
+++ src/app/api/profile/update/route.js
@@ -3,38 +3,58 @@ import { connectDB } from "@/lib/db";
 import { catchError, response } from "@/lib/helperFunction";
 import UserModel from "@/models/User.model";
 
-export async function PUT(request){
+export async function PUT(request) {
   try {
-    await connectDB()
-    const auth = await isAuthenticated("user")
-    if(!auth.isAuth){
-      return response(false, 401, "Unauthorized")
+    await connectDB();
+    const auth = await isAuthenticated("user");
+    if (!auth.isAuth) {
+      return response(false, 401, "Unauthorized");
     }
 
     const userId = auth.userId;
     const user = await UserModel.findById(userId);
 
-    if(!user){
-      return response(false, 404, "User not found.")
+    if (!user) {
+      return response(false, 404, "User not found.");
     }
 
-    const formData = await request.formData()
-    const file = formData.get("file")
+    const formData = await request.formData();
+    const file = formData.get("file");
 
-    if(file){
-      const fileBuffer = await file.arrayBuffer()
-      const base64Image = `data:${file.type};base64,${Buffer.from(fileBuffer).toString()}`
+    user.name = formData.get("name");
+    user.phone = formData.get("phone");
+    user.address = formData.get("address");
+
+    if (file) {
+      const fileBuffer = await file.arrayBuffer();
+      const base64Image = `data:${file.type};base64,${Buffer.from(
+        fileBuffer
+      ).toString()}`;
     }
 
-    const uploadFile = await cloudinary.uploader.upload(base64Image,{
-      upload_preset: process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET
-    })
-    
+    const uploadFile = await cloudinary.uploader.upload(base64Image, {
+      upload_preset: process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET,
+    });
 
-    return response(true, 200, "User data.", user)
+    // Remove Old Avatar
+    if (user.avatar.public_id) {
+      await cloudinary.api.delete_resources([user.avatar.public_id]);
+    }
 
+    user.avatar = {
+      url: uploadFile.secure_url,
+      public_id: uploadFile.public_id,
+    };
 
+    await user.save();
+
+    return response(true, 200, "Profile updated succesfully.", {
+      _id: user._id.toString(),
+      role: user.role,
+      name: user.name,
+      avatar: user.avatar,
+    });
   } catch (error) {
-    catchError(error)
+    catchError(error);
   }
 }
