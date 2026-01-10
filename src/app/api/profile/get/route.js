f384a97b5f88f9c9d434511228b90086784ea732
alokMIPL <alok@madhulata.com>
2026-01-10 09:42:28 +0530

    User Profile api code added

---
+++ src/app/api/profile/get/route.js
@@ -0,0 +1,23 @@
+import { isAuthenticated } from "@/lib/authentication";
+import { connectDB } from "@/lib/db";
+import { catchError } from "@/lib/helperFunction";
+import UserModel from "@/models/User.model";
+
+export async function GET(){
+  try {
+    await connectDB()
+    const auth = await isAuthenticated("user")
+    if(!auth.isAuth){
+      return response(false, 401, "Unauthorized")
+    }
+
+    const userId = auth.userId;
+
+    const user = await UserModel.findById(userId).lean()
+
+    return response(true, 200, "User Profile Data", user)
+    
+  } catch (error) {
+    return catchError(error)
+  }
+}
