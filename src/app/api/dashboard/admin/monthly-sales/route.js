5c17c55ca1f615f757fc2961848ba508f685f348
alokMIPL <alok@madhulata.com>
2026-01-23 08:41:39 +0530

    code added

--- src/app/api/dashboard/admin/monthly-sales/route.js
+++ src/app/api/dashboard/admin/monthly-sales/route.js
@@ -1,12 +1,44 @@
 import { isAuthenticated } from "@/lib/authentication";
+import { connectDB } from "@/lib/db";
 import { response } from "@/lib/helperFunction";
+import OrderModel from "@/models/Order.model";
 
-export async function GET(){
+export async function GET() {
   try {
     const auth = await isAuthenticated();
-    if(!auth.isAuth){
+    if (!auth.isAuth) {
       return response(false, 403, "Unauthorized or Not Admin");
     }
+
+    await connectDB();
+
+    const monthlySales = await OrderModel.aggregate([
+      {
+        $match: {
+          deletedAt: null,
+          status: { $in: ["processing", "shipped", "delivered"] },
+        },
+      },
+      {
+        $group: {
+          _id: {
+            year: { $year: "$createdAt" },
+            month: { $month: "$createdAt" },
+          },
+          totalSales: { $sum: "$totalAmount" },
+          orderCount: { $sum: 1 },
+        },
+      },
+      {
+        $sort: { "_id.year": 1, "_id.month": 1 },
+      },
+    ]);
+    return response(
+      true,
+      200,
+      "Monthly sales data fetched successfully.",
+      monthlySales,
+    );
   } catch (error) {
     return catchError(error);
   }
