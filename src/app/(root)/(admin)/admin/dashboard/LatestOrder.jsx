de86b237f1daf361b1d3862ae47e7a777bf23b46
alokMIPL <alok@madhulata.com>
2026-01-24 08:16:37 +0530

    latest order code added

--- src/app/(root)/(admin)/admin/dashboard/LatestOrder.jsx
+++ src/app/(root)/(admin)/admin/dashboard/LatestOrder.jsx
@@ -1,15 +1,51 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import useFetch from "@/hooks/useFetch";
+import Image from "next/image";
+import notFound from "@/assets/images/not-found.png";
 
 const LatestOrder = () => {
+  const [latestOrder, setLatestOrder] = useState();
+  const { data, loading } = useFetch("/api/dashboard/admin/latest-order");
+
+  console.log("Latest order data:", data);
+
+  useEffect(() => {
+    if (data && data.success) {
+      setLatestOrder(data.data);
+    }
+  }, [data]);
+
+  if (loading) {
+    return (
+      <div className="h-full w-full flex justify-center items-center">
+        Loading...
+      </div>
+    );
+  }
+
+  if (!latestOrder || latestOrder.length === 0) {
+    return (
+      <div className="h-full w-full flex justify-center items-center">
+        <Image
+          src={notFound.src}
+          width={notFound.width}
+          height={notFound.height}
+          alt="not-found"
+          className="w-20"
+        />
+      </div>
+    );
+  }
+
   return (
     <Table>
       <TableHeader>
@@ -22,14 +58,14 @@ const LatestOrder = () => {
         </TableRow>
       </TableHeader>
       <TableBody>
-        {Array.from({ length: 20 }).map ((_,i) => (
-          <TableRow key={i} >
-          <TableCell>{`INV00${i + 1}`}</TableCell>
-          <TableCell>{`PAY${i + 1}`}</TableCell>
-          <TableCell>3</TableCell>
-          <TableCell>Pending</TableCell>
-          <TableCell className="text-right" >100</TableCell>
-        </TableRow>
+        {latestOrder?.map((order) => (
+          <TableRow key={order._id}>
+            <TableCell>{order._id}</TableCell>
+            <TableCell>{order.payment_id}</TableCell>
+            <TableCell>{order.products.length}</TableCell>
+            <TableCell>Pending</TableCell>
+            <TableCell className="text-right">100</TableCell>
+          </TableRow>
         ))}
       </TableBody>
     </Table>
